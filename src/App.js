b6630a2154950a1991a2a4ea9332940fa2e85f7c
Karthik S <karthiks001824@gmail.com>
2022-01-01 16:39:05 +0530

    infobox added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
- import { FormControl, Select, MenuItem } from "@mui/material"
+import { FormControl, Select, MenuItem } from "@mui/material"
+
+import InfoBox from './InfoBox'
+import Map from './Map'
 
  //https://disease.sh/v3/covid-19/countries
 
 
 function App() {
-  const [countries, setCountries ] = useState([
-    'USA', 'UK', 'INDIA'
-  ])
+  const [countries, setCountries ] = useState([]);
+  const [country,  setCountry] = useState('worldwide');
+
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -28,12 +31,21 @@ function App() {
     getCountriesData()
   }, [])
 
+const onCountryChange = (event) => {
+  const countryCode  = event.target.value
+
+  setCountry(countryCode);
+}
+
+
   return (
     <div className="App">
       <div className = "app__header" >
         <h1>COVID-19 TRACKER</h1>
         <FormControl className= 'app__dropdown'>
-          <Select variant = "outlined" value = "abc">
+          <Select variant = "outlined" onChange = {onCountryChange} value = { country } >
+
+              <MenuItem value= 'worldwide'>Worldwide</MenuItem>
               {/*Loop through countries and show dropdown*/}
 
               {
@@ -42,25 +54,23 @@ function App() {
                 ))
               }
 
-              {/*<MenuItem value= 'worldwide'>Worldwide</MenuItem>
-              <MenuItem value= 'worldwide'>Option two</MenuItem>
-              <MenuItem value= 'worldwide'>Option Three</MenuItem>
-              <MenuItem value= 'worldwide'>Option Four</MenuItem>*/}
           </Select>
         </FormControl>
       </div>
 
+      <div className = 'app__stats' >
+        <InfoBox title = "Coronavirus Cases" cases = {123} total = {2000} />
+        <InfoBox title = "Recovered" cases = {1234} total = {3000}/>
+        <InfoBox title = "Deaths" cases = {12345} total = {4000} />
 
-      {/*Header => Title + dropdown Field*/}
+      </div>
 
-      {/*Info Boxes*/}
-      {/*Info Boxes*/}
-      {/*Info Boxes*/}
 
       {/*Table */}
       {/*Graph*/}
 
-      {/*Map*/}
+      <Map />
+      
     </div>
   );
 }
